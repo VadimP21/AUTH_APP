ca21bd92a308fe562064b5f314f7640fb227a44e
vadim_p <vadimp_91@mail.ru>
2024-02-09 20:18:02 +0300

    Создан проект

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+"""
+Основная точка входа в проект, содержит стартовые urls
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("accounts/", include("apps.accounts.urls")),
+
+]
