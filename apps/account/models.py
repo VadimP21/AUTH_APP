09f55ab6a91902dd951e011b4b9566c3ec401045
vadim_p <vadimp_91@mail.ru>
2024-02-09 20:54:50 +0300

    Создана модель Пользователь с идентификаторами в формате UUID4.

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,7 +7,7 @@ from django.db import models
 class User(AbstractUser):
     class Meta:
         db_table = "auth_user"
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(
         unique=True,
     )
